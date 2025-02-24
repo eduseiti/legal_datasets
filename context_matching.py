a65f2701218a3c6b4c887080d1f94cf042b6a9b1
Eduardo Seiti de Oliveira <eduseiti@gmail.com>
2025-02-24 18:59:10 +0000

    Checking matching references for RAG k=10 and questions fixes.

--- context_matching.py
+++ context_matching.py
@@ -28,54 +28,65 @@ HIERARCHY_NAMES=[
 
 
 
-def extract_question_references(rag_output_df):
-    questions = rag_output_df['question_number'].to_numpy()
-    contexts = rag_output_df['contexto'].to_numpy()
+def extract_question_references(rag_output_df, 
+                                question_number_field='question_number', 
+                                rag_context_field='contexto',
+                                verbose=True):
+
+    questions = rag_output_df[question_number_field].to_numpy()
+    contexts = rag_output_df[rag_context_field].to_numpy()
 
     questions_references = []
 
     for i in range(len(contexts)):
-        passages_list = ast.literal_eval(contexts[i])
+        if type(contexts[i]) == str:
+            passages_list = ast.literal_eval(contexts[i])
+        else:
+            passages_list = contexts[i]
 
-        for passage in passages_list:
+        if passages_list is not None: 
+            for passage in passages_list:
 
-            passage_info = {}
-            passage_info['question'] = questions[i]        
-            
-            passage_info['path'] = re.split(": \n|: ", passage)[0]
-
-            passage_path_parts = re.split("_", passage_info['path'])
-
-            print(f"passage_path_parts={passage_path_parts}")
-
-            passage_info['nome'] = passage_path_parts[0]
-            
-            current_level = 0
-            for path_details in passage_path_parts[1:]:
-                for j, hierarchy_pattern in enumerate(SPLITTING_HIERARCHY[current_level:]):
-
-                    print(f"current_level={current_level}, j={j}")
-                    
-                    passage_info[HIERARCHY_NAMES[current_level + j]] = ""
-                    
-                    if re.match(hierarchy_pattern, path_details) is not None:
-                        parts = re.split(hierarchy_pattern, path_details)
-
-                        print(f"{HIERARCHY_NAMES[current_level + j]} = {parts}")
-                        
-                        if HIERARCHY_NAMES[current_level + j] == 'capítulo':
-                            passage_info[HIERARCHY_NAMES[current_level + j]] = parts[-1]
-                        else:
-                            passage_info[HIERARCHY_NAMES[current_level + j]] = parts[1]
-
-                        current_level += j + 1
-
-                        break
-
-            for current_level in range(current_level, len(HIERARCHY_NAMES)):
-                passage_info[HIERARCHY_NAMES[current_level]] = ""                        
+                passage_info = {}
+                passage_info['question'] = questions[i]        
                 
-            questions_references.append(passage_info)
+                passage_info['path'] = re.split(": \n|: ", passage)[0]
+
+                passage_path_parts = re.split("_", passage_info['path'])
+
+                if verbose:
+                    print(f"passage_path_parts={passage_path_parts}")
+
+                passage_info['nome'] = passage_path_parts[0]
+                
+                current_level = 0
+                for path_details in passage_path_parts[1:]:
+                    for j, hierarchy_pattern in enumerate(SPLITTING_HIERARCHY[current_level:]):
+
+                        if verbose:
+                            print(f"current_level={current_level}, j={j}")
+                        
+                        passage_info[HIERARCHY_NAMES[current_level + j]] = ""
+                        
+                        if re.match(hierarchy_pattern, path_details) is not None:
+                            parts = re.split(hierarchy_pattern, path_details)
+
+                            if verbose:
+                                print(f"{HIERARCHY_NAMES[current_level + j]} = {parts}")
+                            
+                            if HIERARCHY_NAMES[current_level + j] == 'capítulo':
+                                passage_info[HIERARCHY_NAMES[current_level + j]] = parts[-1]
+                            else:
+                                passage_info[HIERARCHY_NAMES[current_level + j]] = parts[1]
+
+                            current_level += j + 1
+
+                            break
+
+                for current_level in range(current_level, len(HIERARCHY_NAMES)):
+                    passage_info[HIERARCHY_NAMES[current_level]] = ""                        
+                    
+                questions_references.append(passage_info)
 
     return questions_references
 
@@ -83,7 +94,8 @@ def extract_question_references(rag_output_df):
 
 def count_rag_matches(rag_questions_references, 
                       annotated_references, 
-                      file_to_reference):
+                      file_to_reference,
+                      match_mode='all_occurrences'):
 
     context_matches = {}
 
@@ -96,15 +108,37 @@ def count_rag_matches(rag_questions_references,
             context_matches[question_id] = {'correct_references': 0,
                                             'wrong_references': 0,
                                             'missing_references': 0,
-                                            'total_references_count': len(question_annotated_references)}    
+                                            'correct_titles_count': {},
+                                            'wrong_titles_count': {},
+                                            'total_references_count': len(question_annotated_references)}  
         
         if context_document_filename in file_to_reference:
             context_reference = file_to_reference[context_document_filename]
         
-            if context_reference in question_annotated_references:
-                context_matches[question_id]['correct_references'] += 1
+            if match_mode == 'all_occurrences':
+                if context_reference in question_annotated_references:
+                    context_matches[question_id]['correct_references'] += 1
+                else:
+                    context_matches[question_id]['wrong_references'] += 1
+
+            elif match_mode == 'title_occurrences':
+                if context_reference in question_annotated_references:
+                    if context_reference not in context_matches[question_id]['correct_titles_count']:
+                        context_matches[question_id]['correct_titles_count'][context_reference] = 0
+                        context_matches[question_id]['correct_references'] += 1
+
+                    context_matches[question_id]['correct_titles_count'][context_reference] += 1
+                else:
+                    if context_reference not in context_matches[question_id]['wrong_titles_count']:
+                        context_matches[question_id]['wrong_titles_count'][context_reference] = 0
+                        context_matches[question_id]['wrong_references'] += 1
+
+                    context_matches[question_id]['wrong_titles_count'][context_reference] += 1
+
+            elif match_mode == 'complete_occurrences':
+                raise NotImplementedError("\"complete_occurences\" match_mode not yet implemented...")
             else:
-                context_matches[question_id]['wrong_references'] += 1
+                raise ValueError(f"Invalid match_mode == \"{match_mode}\"")
         else:
             context_matches[question_id]['missing_references'] += 1
 
